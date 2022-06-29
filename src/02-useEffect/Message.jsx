6f38445e4e6877a2daaad1469b0e44f2a377042c
Alejo Boga <alejoboga20@gmail.com>
2022-06-29 12:43:01 -0300

    Use cleanup effect to remove listener

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
+	const [coords, setCoords] = useState({ x: 0, y: 0 });
+
 	useEffect(() => {
-		console.log('Mounted');
+		const onMouseMove = ({ x, y }) => {
+			const coords = { x, y };
+			setCoords(coords);
+		};
+
+		window.addEventListener('mousemove', onMouseMove);
 
 		return () => {
-			console.log('Unmounted');
+			window.removeEventListener('mousemove', onMouseMove);
 		};
 	}, []);
 
 	return (
 		<>
 			<h3>User Exists</h3>
+			{JSON.stringify(coords)}
 		</>
 	);
 };
