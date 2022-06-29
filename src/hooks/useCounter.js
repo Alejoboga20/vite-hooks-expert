1b07bf98bba077097af7b650a6db32720a229698
Alejo Boga <alejoboga20@gmail.com>
2022-06-29 11:06:13 -0300

    Create custom hook for counter

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useCounter = (initialCounter = 0) => {
+	const [counter, setCounter] = useState(initialCounter);
+
+	const increment = (value = 1) => {
+		setCounter(counter + value);
+	};
+
+	const decrement = (value = 1) => {
+		if (counter === 0) return;
+		setCounter(counter - value);
+	};
+
+	const reset = () => {
+		setCounter(initialCounter);
+	};
+
+	return [counter, increment, decrement, reset];
+};
