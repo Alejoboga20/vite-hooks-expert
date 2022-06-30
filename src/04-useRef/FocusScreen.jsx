03beb54b3b99c8379e941903856f0c0e31f5dda2
Alejo Boga <alejoboga20@gmail.com>
2022-06-30 12:45:59 -0300

    Use use

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+
+export const FocusScreen = () => {
+	const inputRef = useRef();
+
+	const onClick = () => {
+		inputRef.current.select();
+	};
+
+	return (
+		<>
+			<h1>FocusScreen</h1>
+			<hr />
+
+			<input className='form-control' type='text' placeholder='Name' ref={inputRef} />
+
+			<button className='btn btn-primary mt-2' onClick={onClick}>
+				Set Focus
+			</button>
+		</>
+	);
+};
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<MultipleCustomHooks />
+		<FocusScreen />
 	</React.StrictMode>
 );
