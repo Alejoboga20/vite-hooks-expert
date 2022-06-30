505c73d09d0ce7a2a94cf0b8877028dedb66e4c5
Alejo Boga <alejoboga20@gmail.com>
2022-06-30 12:57:18 -0300

    useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,24 @@
+import { LoadingQuote, Quote } from '../components';
+import { useCounter, useFetch } from '../hooks';
+
+export const Layout = () => {
+	const [counter, increment, decrement] = useCounter(1);
+	const { isLoading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+	const { quote, author } = !!data && data[0];
+
+	return (
+		<>
+			<h1>Breaking bad quotes - Layout</h1>
+			<hr />
+
+			{isLoading ? <LoadingQuote /> : <Quote author={author} quote={quote} />}
+
+			<button className='btn btn-secondary' onClick={() => decrement(1)}>
+				Previous Quote
+			</button>
+			<button className='btn btn-primary' onClick={() => increment(1)}>
+				Next Quote
+			</button>
+		</>
+	);
+};
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,8 +1,30 @@
+import { useLayoutEffect, useRef, useState } from 'react';
+
 export const Quote = ({ quote, author }) => {
+	const pRef = useRef();
+	const [quoteSize, setQuoteSize] = useState({ width: 0, height: 0 });
+
+	useLayoutEffect(() => {
+		const { width, height } = pRef.current.getBoundingClientRect();
+		setQuoteSize({ width, height });
+	}, []);
+
 	return (
-		<blockquote className='blockquote text-end'>
-			<p className='mb-2'>{quote}</p>
-			<footer className='blockquote-footer mt-1'>{author}</footer>
-		</blockquote>
+		<>
+			<blockquote
+				className='blockquote text-end'
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<p className='mb-2' ref={pRef}>
+					{quote}
+				</p>
+				<footer className='blockquote-footer mt-1'>{author}</footer>
+			</blockquote>
+
+			<code>{JSON.stringify(quoteSize)}</code>
+		</>
 	);
 };
