e23197209211ab116287a92d6034d8706c065d47
Alejo Boga <alejoboga20@gmail.com>
2022-06-30 10:51:09 -0300

    Create component to use custom hook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -2,15 +2,23 @@ import { useFetch } from '../hooks/useFetch';
 
 export const MultipleCustomHooks = () => {
 	const { isLoading, data, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1');
-
-	if (isLoading) return <p>Loading...</p>;
+	const { quote, author } = !!data && data[0];
 
 	return (
 		<>
 			<h1>Breaking bad quotes</h1>
 			<hr />
+
+			{isLoading ? (
+				<div className='alert alert-info text-center'>Loading...</div>
+			) : (
+				<blockquote className='blockquote text-end'>
+					<p className='mb-2'>{quote}</p>
+					<footer className='blockquote-footer mt-1'>{author}</footer>
+				</blockquote>
+			)}
+
+			<button className='btn btn-primary'>Next Quote</button>
 		</>
 	);
 };
-
-//https://www.breakingbadapi.com/api/quotes/1
