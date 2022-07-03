c1f0cff2ce915fe8db12632aa73696b6a50fc373
Alejo Boga <alejoboga20@gmail.com>
2022-07-03 19:38:36 -0300

    Solve challenge

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Padre } from '../07-ChallengeMemo/Padre';
 import { CounterApp } from './01-useState/CounterApp';
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
@@ -12,8 +13,4 @@ import { MemoHook } from './06-Memo/MemoHook';
 import { Memorize } from './06-Memo/Memorize';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<CallbackHook />
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<Padre />);
