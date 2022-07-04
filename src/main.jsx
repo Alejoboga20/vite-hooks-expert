499e7b2f449689b43c9bc0824b557ea41c2e4624
Alejo Boga <alejoboga20@gmail.com>
2022-07-04 11:51:13 -0300

    Setup app to use context from router

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,12 @@ import { MemoHook } from './06-Memo/MemoHook';
 import { Memorize } from './06-Memo/Memorize';
 //import './08-useReducer/intro-reducer';
 import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<TodoApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<MainApp />
+	</BrowserRouter>
+);
