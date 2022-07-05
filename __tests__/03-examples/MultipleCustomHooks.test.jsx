2a3cf275e93f0cfc50955c460b47c45d8c9e1d85
Alejo Boga <alejoboga20@gmail.com>
2022-07-05 10:02:12 -0300

    Test in MultipleHooks

--- __tests__/03-examples/MultipleCustomHooks.test.jsx
+++ __tests__/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MultipleCustomHooks } from '../../src/03-examples/MultipleCustomHooks';
+import { useCounter } from '../../src/hooks/useCounter';
+import { useFetch } from '../../src/hooks/useFetch';
+
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
+
+const mockIncrement = jest.fn();
 
 describe('MultipleCustomHook Tests', () => {
+	beforeEach(() => {
+		useCounter.mockReturnValue({ counter: 1, increment: mockIncrement });
+	});
+
 	test('should show default component', () => {
+		useFetch.mockReturnValue({
+			isLoading: true,
+			data: null,
+			hasError: null,
+		});
+
 		render(<MultipleCustomHooks />);
 
 		expect(screen.getByText('Loading...')).toBeTruthy();
@@ -10,5 +27,34 @@ describe('MultipleCustomHook Tests', () => {
 
 		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
 		expect(nextButton.disabled).toBeTruthy();
+	});
+
+	test('should show a quote', () => {
+		useFetch.mockReturnValue({
+			isLoading: false,
+			data: [{ author: 'Alejo', quote: 'Mocking fetch' }],
+			hasError: null,
+		});
+
+		render(<MultipleCustomHooks />);
+		expect(screen.getByText('Mocking fetch')).toBeTruthy();
+		expect(screen.getByText('Alejo')).toBeTruthy();
+
+		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
+		expect(nextButton.disabled).toBeFalsy();
+	});
+
+	test('should call increment', () => {
+		useFetch.mockReturnValue({
+			isLoading: false,
+			data: [{ author: 'Alejo', quote: 'Mocking fetch' }],
+			hasError: null,
+		});
+
+		render(<MultipleCustomHooks />);
+		const nextButton = screen.getByRole('button', { name: 'Next Quote' });
+
+		fireEvent.click(nextButton);
+		expect(mockIncrement).toHaveBeenCalled();
 	});
 });
